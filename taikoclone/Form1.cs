2a4471146bf96d5e11c85e15402563f83c4433a6
KermitNuggies <50683296+TextAdventurer12@users.noreply.github.com>
2024-05-14 11:49:16 +1200

    Add files via upload

---
+++ taikoclone/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace taikoclone
+{
+    public partial class Form1 : Form
+    {
+        bool leftKey;
+        bool rightKey;
+        double currentTime;
+        Graphics graphics;
+        List<HitObject> hitObjects;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pictureBoxClickCircle.CreateGraphics();
+            hitObjects = new List<HitObject>
+            {
+                new HitObject(2000),
+            };
+            foreach (var hitObject in hitObjects)
+                this.Controls.Add(hitObject.box);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+                leftKey = true;
+            if (e.KeyCode == Keys.T)
+                rightKey = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+                leftKey = false;
+            if (e.KeyCode == Keys.T)
+                rightKey = false;
+        }
+
+        private void GameUpdate_Tick(object sender, EventArgs e)
+        {
+            currentTime += GameUpdate.Interval;
+            label1.Text = currentTime.ToString();
+            foreach (HitObject hitObject in hitObjects)
+                hitObject.Draw(currentTime);
+            Rectangle rect = new Rectangle(0, 0, 100, 100);
+            graphics.FillPie(new SolidBrush(leftKey ? Color.Red : Color.DarkGray), rect, 90, 180);
+            graphics.FillPie(new SolidBrush(rightKey ? Color.Blue : Color.DarkGray), rect, 270, 180);
+        }
+    }
+}
---
+++ taikoclone/HitObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taikoclone
+{
+    public class HitObject
+    {
+        /// <summary>
+        /// The time at which this object should be hit
+        /// </summary>
+        private double time;
+
+        private double preempt = 500;
+        private double playfieldStart = 112;
+        private double playfieldEnd = 700;
+        private double hitWindow = 100;
+        private int y = 12;
+        private int radius = 25;
+        private Image sprite;
+        public PictureBox box;
+
+        public HitObject(double time)
+        {
+            this.time = time;
+            sprite = Image.FromFile("../../taikohitcircle.png");
+            box = new PictureBox();
+            box.Image = sprite;
+            box.Location = new System.Drawing.Point(0, 0);
+            box.Size = new System.Drawing.Size(radius * 2, radius * 2);
+            box.Visible = true;
+        }
+        private double xPosition(double remainingTime)
+        {
+            // proportion of duration remaining
+            double dR = remainingTime / preempt;
+            return (1 - dR) * playfieldStart + dR * playfieldEnd;
+        }
+        public void Draw(double currentTime)
+        {
+            double remainingTime = time - currentTime;
+            if (remainingTime < -hitWindow || remainingTime > preempt)
+            {
+                //box.Visible = false;
+                return;
+            }
+            else box.Visible = true;
+            int x = (int)xPosition(remainingTime);
+            box.Location = new Point(x, y);
+        }
+    }
+}
